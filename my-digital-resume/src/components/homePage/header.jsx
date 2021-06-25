9c2b4feeae1ec7e5782d47ba8fac1d2100b027e4
usama455 <muhammad.usama@surveyauto.com>
2021-06-25 10:52:14 +0500

    reactstrap added

--- my-digital-resume/src/components/homePage/header.jsx
+++ my-digital-resume/src/components/homePage/header.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import { Row, Col, Image } from "antd";
+// import { Row, Col, Image } from "antd";
+
+import { Container, Row, Col } from "reactstrap";
 import headerImage from "../../media/headerImage.png";
 
 const Header = () => {
 	return (
 		<div>
-			<Row>
-				<Col span={12}>
-					<h1 className="textMid">HELLO, I AM THE ENGINEER WHO READS DOCUMENTATION</h1>
-				</Col>
-				<Col span={12}>
-					<Image width={500} preview={false} src={headerImage} />
-
-					{/* <img src={headerImage} alt="logo" /> */}
-				</Col>
-			</Row>
+			<div className="header-bg">
+				<Container>
+					<Row>
+						<Col className="box"></Col>
+						<Col className="box">
+							<h1 className="textMid">HELLO, I AM THE ENGINEER WHO READS DOCUMENTATION</h1>
+						</Col>
+					</Row>
+				</Container>
+				{/* <Row>
+					<Col className="box" span={12}></Col>
+					<Col className="box" span={12}>
+						<h1 className="textMid">HELLO, I AM THE ENGINEER WHO READS DOCUMENTATION</h1>
+						<Image width={500} preview={false} src={headerImage} />
+					</Col>
+				</Row> */}
+			</div>
 		</div>
 	);
 };

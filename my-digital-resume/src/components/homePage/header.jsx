6b5c08f001eb6f092ee03b902ed23f44ea17b128
usama455 <muhammad.usama@surveyauto.com>
2021-06-25 17:01:46 +0500

    bootstrap tryout

--- my-digital-resume/src/components/homePage/header.jsx
+++ my-digital-resume/src/components/homePage/header.jsx
@@ -1,29 +1,33 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
-// import { Row, Col, Image } from "antd";
+import { Row, Col, Image } from "antd";
 
-import { Container, Row, Col } from "reactstrap";
+// import { Container, Row, Col } from "reactstrap";
 import headerImage from "../../media/headerImage.png";
 
 const Header = () => {
 	return (
 		<div>
-			<div className="header-bg">
-				<Container>
-					<Row>
-						<Col className="box"></Col>
-						<Col className="box">
-							<h1 className="textMid">HELLO, I AM THE ENGINEER WHO READS DOCUMENTATION</h1>
-						</Col>
-					</Row>
-				</Container>
-				{/* <Row>
+			<div class="container header-bg">
+				{/* <div className="header-bg"> */}
+				<div className="row">
+					<div className="col box">
+						{/* <Image width={100} preview={false} src={headerImage} /> */}
+						<img src={headerImage} className="img-fluid" alt="Responsive image" />
+					</div>
+					<div className="col box">
+						<h1 className="textMid">HELLO, I AM THE ENGINEER WHO READS DOCUMENTATION</h1>
+					</div>
+				</div>
+			</div>
+			{/* <Row>
 					<Col className="box" span={12}></Col>
 					<Col className="box" span={12}>
 						<h1 className="textMid">HELLO, I AM THE ENGINEER WHO READS DOCUMENTATION</h1>
 						<Image width={500} preview={false} src={headerImage} />
 					</Col>
 				</Row> */}
-			</div>
+			{/* </div> */}
 		</div>
 	);
 };

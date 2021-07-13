2057d1141eab7b46a64557ff6306ee33c7b82e61
Usama <muhammad.usama455@gmail.com>
2021-07-13 23:27:27 +0500

    package.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
